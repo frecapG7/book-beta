470a0ebc7a5b092fc1b918597b65151133a2d60c
frecape <frecape@lifesquare.fr>
2022-02-15 11:55:18 +0100

    Search book page

--- front-book/src/books/SearchBook.jsx
+++ front-book/src/books/SearchBook.jsx
@@ -1,6 +1,8 @@
-import { Button, Container, Grid, Paper, Typography } from "@mui/material"
+import { Button, Container, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import FormTextField from "../forms/FormTextField";
 import NavBar from "../navigation/NavBar";
 import api from "../utils/api";
@@ -9,6 +11,8 @@ const SearchBook = () => {
 
     const { control, handleSubmit } = useForm();
     const [searchResults, setSearchResults] = useState([]);
+    const { t } = useTranslation(['books', 'commons']);
+    const navigate = useNavigate();
 
     useEffect(() => {
         // api.get('/test').then(resp => {
@@ -26,53 +30,87 @@ const SearchBook = () => {
             console.log(err);
         })
     }
+
+    const onPageNumberChanges = (event, nextPage) => {
+        console.log('TODO: set search params + call backend');
+        console.log(nextPage);
+    }
+
+    const onPageSizeChanges = (event) => {
+        console.log(event.target.value)
+    }
+
     return (
         <NavBar>
             <Container maxWidth={false}>
-                <Typography variant="h4">TODO</Typography>
-
-                <Paper>
+                <Typography variant="h2">{t('books:searchBooks', 'Search Books')}</Typography>
+                <Paper sx={{py: 5}}>
                     <form noValidate onSubmit={handleSubmit(onSubmit)}>
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <FormTextField
-                                    label="search"
-                                    name="search"
-                                    control={control} />
+                        <Grid container sx={{ justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+                            <Grid item xs={12} sx={{ justifyContent: 'center', display: 'flex'}}>
+                                <Grid item xs={12} sm={6} sx={{ py: 2 }}>
+                                    <FormTextField
+                                        label={t('commons:search')}
+                                        name="search"
+                                        control={control} />
 
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <Button
-                                    type="submit"
-                                    variant="contained"
-                                >Search</Button>
+                            <Grid item xs={12} sx={{ justifyContent: 'center', display: 'flex'}}>
+                                <Grid item xs={12} sm={6}>
+                                    <Button
+                                        type="submit"
+                                        variant="contained"
+                                        fullWidth
+                                    >{t('commons:search', 'Search')}</Button>
+                                </Grid>
                             </Grid>
+
                         </Grid>
                     </form>
                 </Paper>
-                <Paper>
-                    <Grid container>
-                        {searchResults?.map(searchResult => (
-                            <Grid item xs={12}>
-                                <Grid item xs={6}>
-                                    TODO
-                                </Grid>
-                            </Grid>
-                        ))}
-                    </Grid>
 
-                </Paper>
+                <TableContainer component={Paper} sx={{my: 5, border: 1, borderRadius: 5}}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><Typography variant="h4">{t('books:title', 'Title')}</Typography></TableCell>
+                                <TableCell><Typography variant="h4">{t('books:author', 'Author')}</Typography></TableCell>
+                                <TableCell><Typography variant="h4">{t('books:publictionDate', 'Publication date')}</Typography></TableCell>
+                                <TableCell><Typography variant="h4"></Typography></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {searchResults?.map((result) => (
+                                <TableRow key={result.id}>
+                                    <TableCell><Typography variant="h6">{result.title}</Typography></TableCell>
+                                    <TableCell><Typography variant="h6">{result.author}</Typography></TableCell>
+                                    <TableCell><Typography variant="h6">{result.publishDate}</Typography></TableCell>
+                                </TableRow>
+                            ))}
 
-                <Container>
-                    <Grid container>
-                        {searchResults?.map((searchResult) => (
-                            <Grid key={searchResult.id} item xs={12}>
-                                <Typography variant="h2">{searchResult.title}</Typography>
-                            </Grid>
-                        )
-                        )}
-                    </Grid>
-                </Container>
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                {/* TODO: pagination in backend */}
+                                <TablePagination count={0}
+                                    onPageChange={onPageNumberChanges}
+                                    page={1}
+                                    rowsPerPage={25}
+                                    onRowsPerPageChange={onPageSizeChanges}
+                                    rowsPerPageOptions={[10, 25, 50]}>
+
+
+                                </TablePagination>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
+
+                <Button fullWidth variant="contained" onClick={() => navigate('/books/add')}>
+                        {t('books:addNew', 'Add new')}
+                </Button>
+
             </Container >
         </NavBar>
     )
